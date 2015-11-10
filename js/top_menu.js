dbf546f7ba65ac701f90d2e79bba7abd2b020246
tatia-kom <komarovats93@gmail.com>
2015-11-10 15:25:06 +0300

    Шапка при скроле

--- js/top_menu.js
+++ js/top_menu.js
@@ -2,7 +2,21 @@ $(document).scroll(function() {
 		var scr = $(document).scrollTop();
 		var h = $('header').height() + 2;
 		var top = h - scr;
-		if (top < 0) {top = 0;}
+		if (top < 0) {
+			top = 0;
+			$('nav').css('min-height', '63px');
+			$('.navbar-brand').show();
+			$('nav ul').css('width', 'auto');
+			$('nav ul').css('margin-top', '10px');
+			$('.navbar-header button').css('margin-top', '14px');
+		}
+		else {
+			$('nav').css('min-height', 'auto');
+			$('.navbar-brand').css('display', 'none');
+			$('nav ul').css('width', '100%');
+			$('nav ul').css('margin-top', '0px');
+			$('.navbar-header button').css('margin-top', '3px');
+		}
 		$('nav').css('top', top);
 		var logo_top = $(window).height()/2 - 40;
 		$('.basket_logo').css('top', logo_top);
